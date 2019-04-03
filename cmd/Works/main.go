b24cca22456a05abf9653005aba1d6f4c5823c9b
maojinbiao <maojinbiao@maka.im>
2019-04-03 21:20:10 +0800

    填写相对应信息

--- cmd/Works/main.go
+++ cmd/Works/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/micro/go-micro"
 	"github.com/micro/go-plugins/registry/etcdv3"
 	"go-micro/cmd/Works/proto"
+	model2 "go-micro/cmd/auditConf/model"
+	"go-micro/cmd/auditConf/proto"
 	"go-micro/cmd/works/model"
 	"log"
 	"time"
@@ -25,6 +27,8 @@ func main() {
 	_ = worksAudit.RegisterWorksAuditHandler(service.Server(), model.NewWorksAuditModel())
 	_ = worksAudit.RegisterWorksHandler(service.Server(), model.NewWorksModel())
 	_ = worksAudit.RegisterDailyPvUvHandler(service.Server(), model.NewDailyPvUvModel())
+	// 临时兼容注册到一个服务中
+	_ = auditConf.RegisterAuditConfHandler(service.Server(), model2.NewAuditConfModel())
 
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
