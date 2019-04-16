d46bfa948cee7784a50d5092f2df18cfa24ca5a0
zhangyanhe <zhangyanhe@maka.im>
2019-04-16 17:27:34 +0800

    修改脚本

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+lsof -i:8082 | awk '{print $2}' | xargs kill -9
+lsof -i:50001 | awk '{print $2}' | xargs kill -9
+lsof -i:50002 | awk '{print $2}' | xargs kill -9
+
 cp ./config-example/config-eaxmple.go  ./config/config-eaxmple.go
 go build -i -o micro  ./vendor/github.com/micro/micro/main.go
-./micro --server=grpc --client=grpc --transport=grpc --broker=grpc --registry=etcdv3  web
+ nohup ./micro --server=grpc --client=grpc --transport=grpc --broker=grpc --registry=etcdv3  web &
 
 go build -i -o works  cmd/works/main.go
-./works  --server_address=0.0.0.0:50001
+nohup ./works  --server_address=0.0.0.0:50001 &
 
-go build -i -o  template cmd/template/main.go
-./template
+go build -i -o  template cmd/template/main.go &
+nohup ./template --server_address=0.0.0.0:50002 &
