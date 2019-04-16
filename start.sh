4d982d0613dce413bd932eff7791d08b7a34f9f9
zhangyanhe <zhangyanhe@maka.im>
2019-04-16 17:10:52 +0800

    更新启动脚本

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+cp ./config-example/config-eaxmple.go  ./config/config-eaxmple.go
 go build -i -o micro  ./vendor/github.com/micro/micro/main.go
 ./micro --server=grpc --client=grpc --transport=grpc --broker=grpc --registry=etcdv3  web
 
 go build -i -o works  cmd/works/main.go
-./run  --server_address=0.0.0.0:50001
+./works  --server_address=0.0.0.0:50001
 
 go build -i -o  template cmd/template/main.go
 ./template
