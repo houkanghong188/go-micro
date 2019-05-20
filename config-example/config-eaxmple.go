c724f005fd287741f9970871b9cf0a872af5acc0
Dragonbuf <sylarzhang@outlook.com>
2019-05-20 16:46:43 +0800

    Update config-eaxmple.go

--- config-example/config-eaxmple.go
+++ config-example/config-eaxmple.go
@@ -20,18 +20,18 @@ var (
 	RedisMaxIdleTimeout = 5 * time.Second
 
 	// mysql
-	MysqlMasterDns          = "testgroup:testgroupM1@(rm-2zezj9n2lv83nl8x4o.mysql.rds.aliyuncs.com:3306)/makaplatv4?charset=utf8&parseTime=True&loc=Local"
+	MysqlMasterDns          = ""
 	MysqlMasterMaxIdleConns = 2
 	MysqlMasterMaxOpenConns = 3
 	MasterLogSql            = false
 
-	MysqlSlaverDns          = "testgroup:testgroupM1@(rm-2zezj9n2lv83nl8x4o.mysql.rds.aliyuncs.com:3306)/makaplatv4?charset=utf8&parseTime=True&loc=Local"
+	MysqlSlaverDns          = ""
 	MysqlSlaverMaxIdleConns = 2
 	MysqlSlaverMaxOpenConns = 3
 	SlaverLogSql            = false
 
 	// mysqlData
-	MysqlStatisticsDns          = "testgroup:testgroupM1@(rm-2zezj9n2lv83nl8x4o.mysql.rds.aliyuncs.com:3306)/makadata?charset=utf8&parseTime=True&loc=Local"
+	MysqlStatisticsDns          = ""
 	MysqlStatisticsMaxIdleConns = 2
 	MysqlStatisticsMaxOpenConns = 3
 	StatisticsLogSql            = false
